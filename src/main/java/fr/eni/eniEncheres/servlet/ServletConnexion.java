ce8b4bd92b3ac06cca760ef4f8be2876d718e8ae
AmChanson <84091030+AmChanson@users.noreply.github.com>
2021-05-18 14:38:56 +0200

    Create ServletConnexion.java
    Servlet pour se connecter

---
+++ src/main/java/fr/eni/eniEncheres/servlet/ServletConnexion.java
@@ -0,0 +1,78 @@
+package fr.eni.eniEncheres.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.eni.eniEncheres.bo.Utilisateur;
+
+/**
+ * Servlet implementation class ServletConnexion
+ */
+@WebServlet("/ServletConnexion")
+public class ServletConnexion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	 	public static final String LOGIN        = "login";
+	    public static final String MOT_DE_PASSE         = "motDePasse";
+	    public static final String ATT_USER = "utilisateur";
+	    public static final String ATT_SESSION_USER = "sessionUtilisateur";
+	    public static final String VUE              = "/WEB-INF/connexion.jsp";
+	    public static final String VUE_ECHEC              = "/WEB-INF/erreurConnexion.jsp";
+	    public static final String VUE_USER = "/WEB-INF/listeDesEncheres.jsp";
+   
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher(VUE);
+		rd.forward(request, response);
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//création ou récupération de la session
+		HttpSession session = request.getSession();
+		String login;
+		String motDePasse;
+		
+		try {
+			login = request.getParameter(LOGIN);
+			motDePasse = request.getParameter(MOT_DE_PASSE);
+			//UtilisateurManager utilisateurManageur = new UtilisateurManager();
+			//Utilisateur utilisateur = utilisateurManageur.connection(login,motDePasse);
+			Utilisateur utilisateur = new Utilisateur(login, motDePasse, motDePasse, motDePasse, motDePasse, motDePasse, motDePasse, login, motDePasse);
+			if (utilisateur == null) {
+				PrintWriter out = response.getWriter();
+				out.println("Le login ou le mot de passe n'est pas correcte");
+				session.setAttribute(ATT_SESSION_USER, null);
+				RequestDispatcher rd = request.getRequestDispatcher(VUE_ECHEC);
+				rd.forward(request, response);
+			}else {
+				session.setAttribute(ATT_SESSION_USER, utilisateur);
+				RequestDispatcher rd = request.getRequestDispatcher(VUE_USER);
+				rd.forward(request, response);
+			}
+		
+			
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		
+		
+		
+		
+	}
+
+}
