39ee8e21def112a4b1ff0070e8f4d7f9bf3e50f8
MelaJuk <melanie.barriol2021@campus-eni.fr>
2021-05-19 15:20:19 +0200

    modif

--- src/main/java/fr/eni/eniEncheres/bll/UtilisateurManager.java
+++ src/main/java/fr/eni/eniEncheres/bll/UtilisateurManager.java
@@ -1,18 +1,18 @@
 package fr.eni.eniEncheres.bll;
 
+import fr.eni.eniEncheres.dal.DAOFactory;
+import fr.eni.eniEncheres.dal.UtilisateurDAO;
+
 public class UtilisateurManager {
 
 	// ce manager va permettre de faire les traitements attendus sur la classe Utilisateur 
 	
-	// private UtilisateurDAO utilisateurDAO; 
+	private UtilisateurDAO utilisateurDAO; 
 	
-	//le constructeur permet de récupérer une instance  d'utilisateur à travers le DAOFactory
-	
-	/*
 	public UtilisateurManager() {
 		this.utilisateurDAO = DAOFactory.getUtilisateurDAO(); 
 	}
-	*/
+
 	
 	// ajouter les méthodes qui permettent de vérifier les règles à respecter sur les variables 
 		// les méthodes dont va avoir besoin l'utilisateur 
--- src/main/java/fr/eni/eniEncheres/dal/UtilisateurDAO.java
+++ src/main/java/fr/eni/eniEncheres/dal/UtilisateurDAO.java
@@ -1,5 +1,20 @@
 package fr.eni.eniEncheres.dal;
 
+import java.util.List;
+
+import fr.eni.eniEncheres.bo.Utilisateur;
+
 public interface UtilisateurDAO {
+	public List<Utilisateur> selectAll () throws BusinessException; 
+	public void insert(Utilisateur utilisateur) throws BusinessException;
+	public Utilisateur select(String string);
+//	public Utilisateur selectByLoginMotDePasse(String login,String motDePasse) throws BusinessException;
+//	public void delete(int id) throws BusinessException;
+//	public List<Utilisateur> selectAll() throws BusinessException;
+//	public Utilisateur selectById(int id) throws BusinessException;
+	void ajouter(Utilisateur utilisateur);
+
+	//public void ajouter(Utilisateur utilisateur);
+
 
 }
